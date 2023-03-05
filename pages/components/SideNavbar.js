84842c630e09ce720f0a91be0ce24a537ecabc4f
Caryn Chan <carynchan21@gmail.com>
2023-03-06 00:10:34 +0800

    layout changes

--- pages/components/SideNavbar.js
+++ pages/components/SideNavbar.js
@@ -19,7 +19,7 @@ function SideNavbar() {
             aria-hidden="true"
           />
         </Disclosure.Button>
-        <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+        <div className="p-6 w-1/2 h-screen bg-whitelg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start item-center">
             <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
               ASCLoyalty
