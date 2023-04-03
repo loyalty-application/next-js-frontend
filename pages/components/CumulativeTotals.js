f0d7bb8add54d69adf6350c49157ed2316f6949e
soonann <sudo@soonann.dev>
2023-04-03 20:13:37 +0800

    build: cleaned up dependencies

--- pages/components/CumulativeTotals.js
+++ pages/components/CumulativeTotals.js
@@ -1,82 +1,82 @@
 import { useState, useEffect } from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 
 
 const CumulativeTotals = ({ userId }) => {
-  const [totals, setTotals] = useState(null);
-  const BEARER_TOKEN =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IkRhbmVsbGUuTydLb25AZ21haWwuY29tIiwiRmlyc3RfbmFtZSI6IkRhbmVsbGUiLCJMYXN0X25hbWUiOiJPJ0tvbiIsIlVpZCI6IjAwMDA0OGRiLWYzNGYtNDg0MC1hY2EyLTU4ODYzMDQ4YTUzYiIsImV4cCI6MTY4MDU4ODM2OX0.tgtRd6hUQ04gJUxGHbiY6HvcNSb6e8e5bMa_tq09lRw";
+    const [totals, setTotals] = useState(null);
+    const BEARER_TOKEN =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IkRhbmVsbGUuTydLb25AZ21haWwuY29tIiwiRmlyc3RfbmFtZSI6IkRhbmVsbGUiLCJMYXN0X25hbWUiOiJPJ0tvbiIsIlVpZCI6IjAwMDA0OGRiLWYzNGYtNDg0MC1hY2EyLTU4ODYzMDQ4YTUzYiIsImV4cCI6MTY4MDU4ODM2OX0.tgtRd6hUQ04gJUxGHbiY6HvcNSb6e8e5bMa_tq09lRw";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        `http://35.187.241.140:8080/api/v1/user/${userId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
-          },
-        }
-      );
-      const data = await response.json();
-      setTotals({
-        points: data.points,
-        miles: data.miles,
-        cashback: data.cashback,
-      });
-    };
-    fetchData();
-  }, [userId]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(
+                `http://35.187.241.140:8080/api/v1/user/${userId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${BEARER_TOKEN}`,
+                    },
+                }
+            );
+            const data = await response.json();
+            setTotals({
+                points: data.points,
+                miles: data.miles,
+                cashback: data.cashback,
+            });
+        };
+        fetchData();
+    }, [userId]);
 
-  if (!totals) {
-    return <p>Loading...</p>;
-  }
+    if (!totals) {
+        return <p>Loading...</p>;
+    }
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        gap: "20px",
-        flexDirection: "column",
-        paddingTop: "40px",
-        paddingBottom: "40px",
-        paddingRight: "40px",
-      }}
-    >
-      <div style={{ flex: 1 }}>
-        <Card style={{ minWidth: "150px" }}>
-          <CardContent>
-            <Typography gutterBottom>Points</Typography>
-            <Typography variant="h6" component="h5">
-              {totals.points}
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-      <div style={{ flex: 1 }}>
-        <Card style={{ minWidth: "150px" }}>
-          <CardContent>
-            <Typography gutterBottom>Miles</Typography>
-            <Typography variant="h6" component="h6">
-              {totals.miles}
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-      <div style={{ flex: 1 }}>
-        <Card style={{ minWidth: "100px" }}>
-          <CardContent>
-            <Typography gutterBottom>Cashback</Typography>
-            <Typography variant="h6" component="h6">
-              {totals.cashback}
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "flex",
+                gap: "20px",
+                flexDirection: "column",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+                paddingRight: "40px",
+            }}
+        >
+            <div style={{ flex: 1 }}>
+                <Card style={{ minWidth: "150px" }}>
+                    <CardContent>
+                        <Typography gutterBottom>Points</Typography>
+                        <Typography variant="h6" component="h5">
+                            {totals.points}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+            <div style={{ flex: 1 }}>
+                <Card style={{ minWidth: "150px" }}>
+                    <CardContent>
+                        <Typography gutterBottom>Miles</Typography>
+                        <Typography variant="h6" component="h6">
+                            {totals.miles}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+            <div style={{ flex: 1 }}>
+                <Card style={{ minWidth: "100px" }}>
+                    <CardContent>
+                        <Typography gutterBottom>Cashback</Typography>
+                        <Typography variant="h6" component="h6">
+                            {totals.cashback}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
 };
 
 export default CumulativeTotals;
--- pages/components/UserTransactions.js
+++ pages/components/UserTransactions.js
@@ -19,85 +19,85 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 export default function CustomPaginationActionsTable() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [transactions, setTransactions] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [transactions, setTransactions] = useState([]);
 
-      useEffect(() => {
+    useEffect(() => {
         callAPI();
-      }, []);
-    
-      const callAPI = async () => {
+    }, []);
+
+    const callAPI = async () => {
         console.log("calling api");
-      
+
         // CHANGE TOKEN METHOD TO GET TOKEN FROM LOCAL STORAGE
         let token =
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IkRhbmVsbGUuTydLb25AZ21haWwuY29tIiwiRmlyc3RfbmFtZSI6IkRhbmVsbGUiLCJMYXN0X25hbWUiOiJPJ0tvbiIsIlVpZCI6IjAwMDA0OGRiLWYzNGYtNDg0MC1hY2EyLTU4ODYzMDQ4YTUzYiIsImV4cCI6MTY4MDU4ODM2OX0.tgtRd6hUQ04gJUxGHbiY6HvcNSb6e8e5bMa_tq09lRw";
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6IkRhbmVsbGUuTydLb25AZ21haWwuY29tIiwiRmlyc3RfbmFtZSI6IkRhbmVsbGUiLCJMYXN0X25hbWUiOiJPJ0tvbiIsIlVpZCI6IjAwMDA0OGRiLWYzNGYtNDg0MC1hY2EyLTU4ODYzMDQ4YTUzYiIsImV4cCI6MTY4MDU4ODM2OX0.tgtRd6hUQ04gJUxGHbiY6HvcNSb6e8e5bMa_tq09lRw";
         try {
-          const userId = "6da94a1c-4dd6-4c2c-a35e-8879cc8503c2"; // Replace with the actual user ID
-          const res = await fetch(`http://35.187.241.140:8080/api/v1/transaction/${userId}`, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          console.log(res);
-          console.log("inside the try block");
-          const data = await res.json();
-          console.log(data);
-          setTransactions(data);
-          console.log("ending try block")
+            const userId = "6da94a1c-4dd6-4c2c-a35e-8879cc8503c2"; // Replace with the actual user ID
+            const res = await fetch(`http://35.187.241.140:8080/api/v1/transaction/${userId}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log(res);
+            console.log("inside the try block");
+            const data = await res.json();
+            console.log(data);
+            setTransactions(data);
+            console.log("ending try block")
         } catch (err) {
-          console.log("error");
-          console.log(err.message);
+            console.log("error");
+            console.log(err.message);
         }
-      };
+    };
 
-      const getRewardValue = (points, miles, cashback) => {
+    const getRewardValue = (points, miles, cashback) => {
         if (points === 0 && miles === 0 && cashback !== 0) {
-          return `$${cashback.toFixed(2)} cashback`;
+            return `$${cashback.toFixed(2)} cashback`;
         } else if (points !== 0 && miles === 0 && cashback === 0) {
-          return `${points} points`;
+            return `${points} points`;
         } else if (points === 0 && miles !== 0 && cashback === 0) {
-          return `${miles} miles`;
+            return `${miles} miles`;
         } else {
-          return "-";
+            return "-";
         }
-      } 
+    }
 
-  return (
-    <div className="pt-5 p-20 bg-[#F7F8F9]">
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Transaction ID</TableCell>
-            <TableCell >Transaction Date</TableCell>
-            <TableCell >Amount</TableCell>
-            <TableCell >Merchant</TableCell>
-            <TableCell >Reward</TableCell>
-            {/* <TableCell align="right">Miles</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {transactions.map((transaction) => (
-            <TableRow key={transaction.id}>
-              <TableCell component="th" scope="row">
-                {transaction.transaction_id}
-              </TableCell>
-              <TableCell>
-                {transaction.transaction_date}
-              </TableCell>
-              <TableCell >{transaction.amount}</TableCell>
-              <TableCell >{transaction.merchant}</TableCell>
-              <TableCell>{getRewardValue(transaction.points, transaction.miles, transaction.cashback)}</TableCell>
-    
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-  );
+    return (
+        <div className="pt-5 p-20 bg-[#F7F8F9]">
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Transaction ID</TableCell>
+                            <TableCell >Transaction Date</TableCell>
+                            <TableCell >Amount</TableCell>
+                            <TableCell >Merchant</TableCell>
+                            <TableCell >Reward</TableCell>
+                            {/* <TableCell align="right">Miles</TableCell> */}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {!transactions ? "" : transactions.map((transaction) => (
+                            <TableRow key={transaction.id}>
+                                <TableCell component="th" scope="row">
+                                    {transaction.transaction_id}
+                                </TableCell>
+                                <TableCell>
+                                    {transaction.transaction_date}
+                                </TableCell>
+                                <TableCell >{transaction.amount}</TableCell>
+                                <TableCell >{transaction.merchant}</TableCell>
+                                <TableCell>{getRewardValue(transaction.points, transaction.miles, transaction.cashback)}</TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
 }
